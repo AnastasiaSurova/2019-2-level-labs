0983c04763c75e268c556b569145d3ba3cf45d78
AnastasiaSurova <55386467+AnastasiaSurova@users.noreply.github.com>
2019-09-16 15:28:00 +0500

    added str

--- lab_1/main.py
+++ lab_1/main.py
@@ -8,7 +8,7 @@ def calculate_frequences(text: str) -> dict:
     """
     Calculates number of times each word appears in the text
     """
-    'A cat is white'
+    l = 'A cat is white'
     pass
 
 def filter_stop_words(frequencies: dict, stop_words: tuple) -> dict:
