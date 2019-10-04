e8a7b2c1be98c931a4e8c40d826565738ad811e7
AnastasiaSurova <asurova909@gmail.com>
2019-10-05 01:11:41 +0500

    Completed 3 func. Completed additional func.

--- lab_1/main.py
+++ lab_1/main.py
@@ -4,39 +4,43 @@ Count frequencies dictionary by the given arbitrary text
 """
 lines_limit = 5
 path_to_file = 'The Cactus.txt'
+
+
 def read_from_file(path_to_file: str, lines_limit: int) -> str:
-    opened_text = open(path_to_file, 'r')  # открывает файл с текстом для чтения, текстовый файл находится в папке с файлом с кодом
+    opened_text = open(path_to_file, 'r')
     text_read = ''
     for number in range(lines_limit):
         text_read += opened_text.readline()
-    #text_read = opened_text.readlines()[:lines_limit + 1]
     opened_text.close()
     return text_read
 
+
 text = read_from_file(path_to_file, lines_limit)
+
 
 def calculate_frequences(text: str) -> dict:
     """
     Calculates number of times each word appears in the text
     """
     frequencies = {}
-    if type(text) == str:
-        text = text.lower()  # меняет регистр текста на нижний
-        text_splitted = text.replace("'", " ").split()  # делим  строку на элемены по пробелам
+    if str == type(text):
+        text = text.lower()
+        text_splitted = text.replace("'", " ").split()
         new_text = []
-        punct_marks = [ '@', '$', '%','*', '&', '^%$' , "—", '"', "'", ".", ":", ";", ",", "/", "\n", "!", "?", "(", ")", "-"]  # список пунктуационных знаков
-        for word in text_splitted:  # проходим по каждому элементу в списке
-            if not word.isdigit() and word not in punct_marks:  # если элемент не цифра и элемент не находится в списке пунктуационных знаков
-                new_word = ''  # создали пустую строку
-                for element in word:  # проходим по каждому элементу в слове
-                    if element.isalpha():  # если элемент буква
-                        new_word += element  # создаем новое слово, добавляем в пустую строку элементы
+        punct_marks = ["@", "$", "%", "*", "&", "^%$", "—", '"', "'", ".",
+                       ":", ";", ",", "/", "\n", "!", "?", "(", ")", "-"]
+        for word in text_splitted:
+            if not word.isdigit() and word not in punct_marks:
+                new_word = ''
+                for element in word:
+                    if element.isalpha():
+                        new_word += element
                 if new_word is not '':
                     new_text.append(new_word)
         for new_word in new_text:
             number_of_word = new_text.count(new_word)
             frequencies[new_word] = number_of_word
-    if text == None:
+    if text is None:
         frequencies = {}
     return frequencies
 
@@ -48,19 +52,19 @@ frequencies = calculate_frequences(text)
 stop_words = ('a', 'hers', 'm', 'am', 'but', 'did', 'there', 'about', 'me', 'by', 'i', 'don', 'he',
               'once', '', 'during', 'out', 'very', 'if', 'with', 'they', 'own', 'an', 'be', 'all',
               'some', 'for', 'do', 'its', 'yours', 'such', 'into', 'of', 'most', 'itself', 'and',
-              't', 'off', 'is', 's', 'so', 'or', 'my', 'as', 'his', 'him', 'each', 'the', 'at', 'not',
+              't', 'off', 'is', 's', 'so', 'or', 'my', 'as', 'his', 'him', 'each', 'the', 'at',
               'are', 'we', 'these', 'your', 'her', 'to', 'up', 'gonna', 'too', 'yes', 'you', 'on',
-              'had', 'that', 'it', 'was', 'in', 'from', 'she', 'been', 'how', 'now', 'no', 'upon', 'this',
-              'one', 'last', 'have', 'when', 'could', 'before', 'any', 'here')
+              'had', 'that', 'it', 'was', 'in', 'from', 'she', 'been', 'how', 'now', 'no', 'upon',
+              'one', 'last', 'have', 'when', 'could', 'before', 'any', 'here', 'this', 'not')
 
 
 def filter_stop_words(frequencies, stop_words):
     """
     Removes all stop words from the given frequencies dictionary
     """
-    if frequencies != None:
+    if frequencies is not None:
         clear_freq = frequencies.copy()
-        if stop_words != None:
+        if stop_words is not None:
             stop_words_list = list(stop_words)
             for key in clear_freq.keys():
                 if type(key) != str:
@@ -74,11 +78,12 @@ def filter_stop_words(frequencies, stop_words):
         return frequencies
 
 
-
 print(filter_stop_words(frequencies, stop_words))
 
 frequencies = filter_stop_words(frequencies, stop_words)
 top_n = 10
+
+
 def get_top_n(frequencies: dict, top_n: int) -> tuple:
     """
     Takes first N popular words
@@ -97,3 +102,16 @@ def get_top_n(frequencies: dict, top_n: int) -> tuple:
 
 
 print(get_top_n(frequencies, top_n))
+
+path_to_file = 'report.txt'
+content = get_top_n(frequencies, top_n)
+
+
+def write_to_file(path_to_file: str, content: tuple):
+    file_opened = open(path_to_file, 'w')
+    for element in content:
+        file_opened.writelines(element)
+        file_opened.writelines('\n')
+    file_opened.close()
+
+write_to_file(path_to_file, content)
