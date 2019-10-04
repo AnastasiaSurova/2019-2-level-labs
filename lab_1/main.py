073ce20ba2e2ce9218dd95eb46b0d993193e29f0
AnastasiaSurova <asurova909@gmail.com>
2019-10-05 02:00:19 +0500

    Added text = str.

--- lab_1/main.py
+++ lab_1/main.py
@@ -15,6 +15,15 @@ def read_from_file(path_to_file: str, lines_limit: int) -> str:
     return text_read
 
 
+text = """The most notable thing about Time is that it is so purely relative. A large amount of reminiscence is, by common consent, conceded to the drowning man; and it is not past belief that one may review an entire courtship while removing one's gloves.
+
+The Cactus Enthusiast, Carl Spitzweg, 1850. That is what Trysdale was doing, standing by a table in his bachelor apartments. On the table stood a singular-looking green plant in a red earthen jar. The plant was one of the species of cacti, and was provided with long, tentacular leaves that perpetually swayed with the slightest breeze with a peculiar beckoning motion.
+The most notable thing about Time is that it is so purely relative. A large amount of reminiscence is, by common consent, conceded to the drowning man; and it is not past belief that one may review an entire courtship while removing one's gloves.
+Trysdale's friend, the brother of the bride, stood at a sideboard complaining at being allowed to drink alone. Both men were in evening dress. White favors like stars upon their coats shone through the gloom of the apartment.
+The Cactus Enthusiast, Carl Spitzweg, 1850. That is what Trysdale was doing, standing by a table in his bachelor apartments. On the table stood a singular-looking green plant in a red earthen jar. The plant was one of the species of cacti, and was provided with long, tentacular leaves that perpetually swayed with the slightest breeze with a peculiar beckoning motion.
+As he slowly unbuttoned his gloves, there passed through Trysdale's mind a swift, scarifying retrospect of the last few hours. It seemed that in his nostrils was still the scent of the flowers that had been banked in odorous masses about the church, and in his ears the lowpitched hum of a thousand well-bred voices, the rustle of crisp garments, and, most insistently recurring, the drawling words of the minister irrevocably binding her to another."""
+
+
 def calculate_frequences(text: str) -> dict:
     """
     Calculates number of times each word appears in the text
@@ -41,6 +50,10 @@ def calculate_frequences(text: str) -> dict:
         frequencies = {}
     return frequencies
 
+
+print(calculate_frequences(text))
+
+frequencies = calculate_frequences(text)
 
 stop_words = ('a', 'hers', 'm', 'am', 'but', 'did', 'there', 'about', 'me', 'by', 'i', 'don', 'he',
               'once', '', 'during', 'out', 'very', 'if', 'with', 'they', 'own', 'an', 'be', 'all',
@@ -71,6 +84,9 @@ def filter_stop_words(frequencies, stop_words):
         return frequencies
 
 
+print(filter_stop_words(frequencies, stop_words))
+
+frequencies = filter_stop_words(frequencies, stop_words)
 top_n = 10
 
 
@@ -91,7 +107,10 @@ def get_top_n(frequencies: dict, top_n: int) -> tuple:
     return tuple_top_words
 
 
+print(get_top_n(frequencies, top_n))
+
 path_to_file = 'report.txt'
+content = get_top_n(frequencies, top_n)
 
 
 def write_to_file(path_to_file: str, content: tuple):
@@ -100,3 +119,5 @@ def write_to_file(path_to_file: str, content: tuple):
         file_opened.writelines(element)
         file_opened.writelines('\n')
     file_opened.close()
+
+write_to_file(path_to_file, content)
